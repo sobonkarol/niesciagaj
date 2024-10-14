6a2a0e79549a927e89b6eea85ad427b1d7c45eb8
karolsobon <karolsobon@aol.com>
2024-10-14 11:46:20 +0200

    Dodanie instalacji ffmpeg

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
 from flask import Flask, render_template, request, send_file
 import os
 import yt_dlp
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Ścieżka, w której będą tymczasowo przechowywane pobrane filmy
+# Ścieżka do przechowywania pobranych plików
 DOWNLOAD_FOLDER = "downloads"
 if not os.path.exists(DOWNLOAD_FOLDER):
     os.makedirs(DOWNLOAD_FOLDER)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/download', methods=['POST'])
 def download_video():
     tweet_url = request.form['tweet_url']
-    custom_file_name = request.form['file_name']
+    custom_file_name = request.form.get('file_name')
 
-    # Ustal domyślną nazwę pliku, jeśli użytkownik nie podał nazwy
+    # Jeśli użytkownik nie poda nazwy, dodaj znacznik czasu do domyślnej nazwy
     if not custom_file_name:
-        custom_file_name = "your_video_from_x"
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        custom_file_name = f"your_video_from_x_{timestamp}"
 
     try:
-        # Konfiguracja yt-dlp
         ydl_opts = {
             'outtmpl': os.path.join(DOWNLOAD_FOLDER, f'{custom_file_name}.%(ext)s'),
             'format': 'bestvideo+bestaudio/best',
@@ -35,12 +34,10 @@ def download_video():
             info_dict = ydl.extract_info(tweet_url, download=True)
             video_file = ydl.prepare_filename(info_dict)
 
-        # Zwrócenie pliku do użytkownika
         return send_file(video_file, as_attachment=True)
 
     except Exception as e:
         return f"Błąd pobierania: {str(e)}"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
